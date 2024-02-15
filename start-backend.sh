f4aeeb338b59bb731f0b008df025e31e7b05b93f
Gregorio Di Stefano <greg.distefano@gmail.com>
2024-02-15 21:00:20 +0100

    Update start-backend.sh

--- start-backend.sh
+++ start-backend.sh
@@ -3,9 +3,10 @@
 
 set -e
 
-docker-compose down --rmi all --remove-orphans && docker-compose up -d --force-recreate 
+docker-compose down --remove-orphans && docker-compose up -d
 sleep 10 # the app will restart a few times until the db and redis are up, there are better ways of doing this.
 
-backend_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' urlshort-app-1)
+app=$(docker ps --format '{{.Names}}' | grep 'urlshort' | grep 'app')
+backend_ip=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $app)
 
 echo "Backend is running at http://$backend_ip:8888 and on http://localhost:8888"
